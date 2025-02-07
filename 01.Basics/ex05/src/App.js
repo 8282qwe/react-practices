c4062c15ac2af464be6399bfa97e17f2a8f5b4aa
Midichi <8282qwe@gmail.com>
2025-02-07 17:05:35 +0900

    02.07 React#1

---
+++ 01.Basics/ex05/src/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+function App() {
+    // const App = document.createElement('div');
+    // App.textContent = 'Hello World!';
+
+    return React.createElement("div",null,"Hello World!!!");
+}
+
+export {App};
